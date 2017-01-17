79d4b9c8e2eafbec65ab0ad7025ce05071219475
FengYuHe <heyufeng8080@gmail.com>
2017-01-17 22:33:03 +0800

    add note for index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const seneca = require('seneca');
 const nconf = require('nconf');
 
+// 根据环境变量获取config文件
 nconf.argv().env();
 const env = process.env.NODE_ENV || 'development';
 const Xlog = require('xyj-logger');
@@ -10,8 +11,10 @@ const logger = Xlog.Logger('logs');
 nconf.file({ file: `config.${env}.json` });
 require('./mongoose');
 
+// 注册seneca服务
 const revikeTokenregister = require('./register/revoke-token-register');
 
+// 所有服务入口log打印
 function log() {
   this.sub('role:auth, cmd:*', (msg) => {
     const inputMsg = { role: msg.role, cmd: msg.cmd, data: msg.data };
@@ -19,6 +22,7 @@ function log() {
   });
 }
 
+// 启动服务
 seneca({ log: { level: 'error+' } })
   .use(log)
   .use(revikeTokenregister)
